9df4457d0f935dac611d85191720d37126da1f76
uflakey <134938478+uflakey@users.noreply.github.com>
2024-02-03 19:42:23 +0200

    ок

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,32 @@
+class Key {
+  private signature: number = Math.random();
+  getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  protected door: boolean = false;
+  private tenants: Person[] = [];
+  constructor(protected key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) this.tenants.push(person);
+  }
+  abstract openDoor(key: Key);
+}
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (this.key.getSignature === key.getSignature) this.door = true;
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +35,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
